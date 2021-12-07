0862d91d5596ab1f6a1ac18c1b4d87b3cc1c9289
Ben Coyne <bencoyne123@yahoo.co.uk>
2021-12-07 17:54:40 +0000

    Second commit (test)

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,7 +1,6 @@
 require 'docking_station'
 
 describe DockingStation do
-  begin docking_station =  DockingStation.new("dock1")
   it "returns nil" do
     expect(docking_station.string).to eq("dock1")
   end
